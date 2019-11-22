a4092ddb84069e78dbb1b462ab7260c5fb1c76ac
Mark Payne <mjpayne8@gmail.com>
2019-11-22 11:32:52 +0000

    Flight class tested and book passenger method added

--- src/main/java/Flight.java
+++ src/main/java/Flight.java
@@ -2,19 +2,57 @@ import java.util.ArrayList;
 
 public class Flight {
 
+    private final String destination;
     private Plane plane;
     private String code;
     private String departurePoint;
-    private String arrivalPoint;
     private String departureTime;
     private ArrayList<Passenger> passengers;
 
-    public Flight(Plane plane, String code, String departurePoint, String arrivalPoint, String departureTime) {
+    public Flight(Plane plane, String code, String departurePoint, String destination, String departureTime) {
         this.plane = plane;
         this.code = code;
         this.departurePoint = departurePoint;
-        this.arrivalPoint = arrivalPoint;
+        this.destination = destination;
         this.departureTime = departureTime;
         this.passengers = new ArrayList<Passenger>();
+    }
+
+    public Plane getPlane() {
+        return this.plane;
+    }
+
+    public String getDeparturePoint() {
+        return this.departurePoint;
+    }
+
+    public String getDestination() {
+        return this.destination;
+    }
+
+    public String getDepartureTime() {
+        return this.departureTime;
+    }
+
+    public int numberOfPassengers() {
+        return this.passengers.size();
+    }
+
+    public int availableSeats() {
+        return this.plane.getCapacity() - this.numberOfPassengers();
+    }
+
+    public boolean isAvailableSeats() {
+        return this.availableSeats() > 0;
+    }
+
+    public void addPassenger(Passenger passenger) {
+        this.passengers.add(passenger);
+    }
+
+    public void bookPassenger(Passenger passenger) {
+        if (this.isAvailableSeats()){
+            this.addPassenger(passenger);
+        }
     }
 }
--- src/test/java/FlightTest.java
+++ src/test/java/FlightTest.java
@@ -1,4 +1,7 @@
 import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class FlightTest {
 
@@ -15,6 +18,68 @@ public class FlightTest {
         passenger1 = new Passenger("Zsolt",1);
         passenger2 = new Passenger("Louise", 2);
         passenger3 = new Passenger("Harrison", 3);
+    }
+
+    @Test
+    public void canGetPlane(){
+        assertEquals(plane, flight.getPlane());
+    }
+
+    @Test
+    public void canGetDeparturePoint(){
+        assertEquals("GLA", flight.getDeparturePoint());
+    }
+
+    @Test
+    public void canGetArrivalPoint(){
+        assertEquals("EDI", flight.getDestination());
+    }
+
+    @Test
+    public void canGetDepartureTime(){
+        assertEquals("12:00", flight.getDepartureTime());
+    }
+
+    @Test
+    public void canGetNumberOfPassengers(){
+        assertEquals(0, flight.numberOfPassengers());
+    }
+
+    @Test
+    public void canAddPassenger(){
+        flight.addPassenger(passenger1);
+        assertEquals(1, flight.numberOfPassengers());
+    }
+
+    @Test
+    public void canGetNumberOfAvailableSeats(){
+        assertEquals(2, flight.availableSeats());
+    }
+
+    @Test
+    public void knowsIfSeatsAvailable(){
+        assertEquals(true, flight.isAvailableSeats());
+    }
+
+    @Test
+    public void knowsIfSeatsAreNotAvailable(){
+        flight.addPassenger(passenger1);
+        flight.addPassenger(passenger2);
+        assertEquals(false, flight.isAvailableSeats());
+    }
+
+    @Test
+    public void booksPassengerIfSeatsAvailable(){
+        flight.bookPassenger(passenger1);
+        assertEquals(1, flight.numberOfPassengers());
+    }
+
+    @Test
+    public void willNotBookPassengerIfFlightFull(){
+        flight.addPassenger(passenger1);
+        flight.addPassenger(passenger2);
+        flight.bookPassenger(passenger3);
+        assertEquals(2, flight.numberOfPassengers());
     }
 
 }
